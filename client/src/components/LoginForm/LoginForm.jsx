d12c1579fbd58498c7bc4a64f5cd851db6fb450f
bcshanken <bcshanken@gmail.com>
2021-04-08 17:12:15 -0400

    added in a form component and container for a user login

---
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return (
+        <form class="userLoginForm">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" class="btn btn-dark">
+          Submit
+        </button>
+          </form>
+    );
+};
+
+export default LoginForm;
